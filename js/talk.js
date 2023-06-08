54596006ce54bf2d8a78659a53c87fa039d1890e
jkjoy <jkjoy@live.cn>
2023-06-08 14:00:39 +0800

    Site updated: 2023-06-08 14:00:38

--- js/talk.js
+++ js/talk.js
@@ -75,7 +75,7 @@ function getNextList() {
 }
 //获取 Memos 总条数
 function getTotal() {
-    var totalUrl = "https://memos.example.com/api/memo/stats?creatorId=101"
+    var totalUrl = memos + "api/memo/stats?creatorId=" + memo.creatorId
     fetch(totalUrl).then(res => res.json()).then(resdata => {
         if (resdata.data) {
             var allnums = resdata.data.length
